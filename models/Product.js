75929e4d16b238f6f040efad0bb6943a27414134
PrashantGM <gmprem14@gmail.com>
2021-12-29 17:44:25 +0545

    implemented aggregation pipeline for averageRatings

--- models/Product.js
+++ models/Product.js
@@ -55,6 +55,10 @@ const ProductSchema = new mongoose.Schema(
       default: 15,
     },
     averageRating: { type: Number, default: 0 },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
@@ -69,5 +73,9 @@ ProductSchema.virtual("reviews", {
   localField: "_id",
   foreignField: "product",
   justOne: false,
+});
+
+ProductSchema.pre("remove", async function (next) {
+  await this.model("Review").deleteMany({ product: this._id });
 });
 module.exports = mongoose.model("Product", ProductSchema);
--- models/Review.js
+++ models/Review.js
@@ -34,4 +34,34 @@ const ReviewSchema = new mongoose.Schema(
 
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+  const result = await this.aggregate([
+    { $match: { product: productId } },
+    {
+      $group: {
+        _id: "$product",
+        averageRating: { $avg: "$rating" },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+  try {
+    await this.model("Product").findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating || 0),
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+ReviewSchema.post("save", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+ReviewSchema.post("remove", async function () {
+  await this.constructor.calculateAverageRating(this.product);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
